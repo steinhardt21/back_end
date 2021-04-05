f96f13348037aa3a8477751302f736454bd3bb66
Alexandru Enache <44054368+steinhardt21@users.noreply.github.com>
2021-04-06 00:30:58 +0200

    Update

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const errorHandler = require('_helpers/error-handler');
 
+const errorHandler = require('_helpers/error-handler');
 const connectDB = require('./config/db')
+
 const path = require('path')
 
-// Connect database
 connectDB();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,13 +18,13 @@ app.use(cors())
 // app.get('/', (req, res) => res.send('API running'))
 
 // Define Routes
-app.use('/api/users', require('./routes/api/users'))
-app.use('/api/auth', require('./routes/api/auth'))
-app.use('/api/profile', require('./routes/api/profile'))
-app.use('/api/projects', require('./routes/api/projects'))
-app.use('/api/candidacy', require('./routes/api/candidacy'))
+app.use('/users', require('./routes/api/users'))
+app.use('/auth', require('./routes/api/auth'))
+app.use('/profile', require('./routes/api/profile'))
+app.use('/projects', require('./routes/api/projects'))
+app.use('/candidacy', require('./routes/api/candidacy'))
 
-app.use('/api/admin', require('./routes/api/admin'))
+app.use('/admin', require('./routes/api/admin'))
 
 app.use(errorHandler);
 
