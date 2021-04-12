ece2135bbb2a75306269decd3ab18868c2dfb2eb
Alexandru Enache <44054368+steinhardt21@users.noreply.github.com>
2021-04-13 00:00:12 +0200

    update

--- server.js
+++ server.js
@@ -3,32 +3,32 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-// const jwt = require('_helpers/jwt');
+
 const errorHandler = require('_helpers/error-handler');
 const connectDB = require('./config/db')
-// Connect database
 
+const path = require('path')
 
 connectDB();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors())
 
+// app.get('/', (req, res) => res.send('API running'))
 
+// Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/projects', require('./routes/api/projects'))
+app.use('/api/candidacy', require('./routes/api/candidacy'))
 
-// api routes
-app.use('/users', require('./routes/api/users'))
-app.use('/auth', require('./routes/api/auth'))
-app.use('/profile', require('./routes/api/profile'))
-app.use('/projects', require('./routes/api/projects'))
-app.use('/candidacy', require('./routes/api/candidacy'))
-
-app.use('/admin', require('./routes/api/admin'))
+app.use('/api/admin', require('./routes/api/admin'))
 
 app.use(errorHandler);
 
-// start server
+// Serve static assets in production
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
 const server = app.listen(port, function () {
     console.log('Server listening on port ' + port);
